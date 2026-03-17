5b0595cc76b3d6f5ad1131b0d9102c51dcf3691a
Emmanuel Villa <emmanuelvv2008@gmail.com>
2026-03-17 15:44:56 -0500

    Verificacion

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const app = express();
 require ('dotenv').config();
 const port = process.env.PORT;
 const PASS = process.env.API_PASSWORD;
+const cors = require('cors');
 
+
+app.use(cors());
 const AutoresRoutes = require('./Autores/AutoresRoutes');
 const LibrosRoutes = require('./Libros/LibrosRoutes');
 const PrestamosRoutes = require('./Prestamos/PrestamoRoutes');
@@ -11,11 +14,13 @@ const UsuariosRoutes = require('./Usuarios/UsuariosRoutes');
 const EmpleadosRoutes = require('./Empleados/EmpleadosRoutes');
 app.use(express.json());
 
+
 app.use('/api',AutoresRoutes);
 app.use('/api', LibrosRoutes);
 app.use('/api', PrestamosRoutes);
 app.use('/api', UsuariosRoutes);
 app.use('/api', EmpleadosRoutes);
+
 
 // Iniciar servidor
 app.listen(port, () => console.log('API arriba!'));
